c9a4a2cc0ea993bd45184350162d3304da06f85e
Kimshubb <issakimanifmk@gmail.com>
2023-11-27 07:24:47 -0800

    attributes as kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,18 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at','updated_at']:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                setattr(self, '__class__', kwargs['__class__'])
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.today()
+                self.updated_at = datetime.today()
 
     def __str__(self):
         clsname = self.__class__.__name__
