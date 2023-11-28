9bbcee8dd3f5cdeda1b21e5e909663a5ce9f0433
Kimshubb <issakimanifmk@gmail.com>
2023-11-28 03:31:11 -0800

    reset

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,22 +16,24 @@ class FileStorage:
 
     def new(self, obj):
         '''create new object sets in __objects obj with key <obj_class_name>.id'''
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        setattr(FileStorage, key, obj)
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
         
     def save(self):
         '''Serialize __objects to JSON file __file_path'''
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as file:
-            json.dump({key: obj.to_dict() for key, obj in FileStorage.__objects.items()}, file)
+            json.dump(objdict, f)
 
     def reload(self):
         '''Deserialize JSON file __file_path to __objects if it exists'''
         try:
             with open(FileStorage.__file_path) as file:
                 obj_dict = json.load(file)
-                for key, obj_data in obj_dict.items():
-                    cls_name, obj_id = key.split('.')
-                    obj = eval(cls_name)(**obj_data)
-                    self.new(obj)
-        except FileNotFoundError:
-            pass
+                for o in obj_dict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o)
+        except IOError:
+             return
