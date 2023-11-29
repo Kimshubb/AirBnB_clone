4e4e93c7ccb99faf85c2afd5f3e9d37e8e0b24c2
Kimshubb <issakimanifmk@gmail.com>
2023-11-29 01:21:25 -0800

    import fixes

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python 3
-from models.engine.file_storage import FileStorage
+from models.engine.file_storage import storage
 '''create unique FileStorage instance for the application'''
 storage = FileStorage()
 '''call the reload method on storage variable'''
