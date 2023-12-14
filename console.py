71fabc007a13f30c543e12171758efd98227d74e
Kimshubb <issakimanifmk@gmail.com>
2023-12-13 23:48:01 -0800

    more classes for console

--- console.py
+++ console.py
@@ -157,6 +157,156 @@ class HBNBCommand(cmd.Cmd):
         obj = all_objs[key]
         setattr(obj, attribute_name, attribute_value)
         obj.save()
+    
+    def do_show_id(self, arg):
+        """retrieves an instance based on its id
+        usage: <class name>. show(<id>)"""
+        args = shlex.split(arg)
+        if len(args) < 2 or args[1] != '.show(' or not args[2].endswith(')'):
+            print("** Invalid syntax. Usage: <class name>.show(<id>) **")
+            return
+        class_name = args[0]
+        obj_id = args[2][:-1]
+        key = "{}.{}".format(class_name, obj_id)
+        all_objs = storage.all()
+
+        if key in all_objs:
+            print(all_objs[key])
+        else:
+            print("** No instance found with ID:{}".format(obj_id))
+self.__class__.do_show_id = self.do_show_id
+
+    def do_all_class(self, arg):
+        """retrieves all instances of a class:
+        usage: <class name.all()>
+        """
+        args = shelext.split(arg)
+        class_name = args[0]
+        all_objs = storage.all()
+        class_instances = [str(obj) for key, obj in all_objs.items() if key.startswith(class_name + '.')]
+        if class_instances:
+            print(class_instances)
+        else:
+            print("** No instances found for class:{}".format(class_name))
+self.__class__.do_all_class = self.do_all_class
+
+    def do_count_class(self, arg):
+        """Retrieves the number of instances of a class
+        usage: <class name>.count()"""
+        class_name = arg.split('.')[0]
+        all_objs = storage.all()
+
+        count = sum(1 for key in all_objs.keys() if key.startswith(class_name + '.'))
+
+        print(count)
+self.__class__.do_count_class = self.do_count_class
+
+    def do_show_id(self, arg):
+        """
+        Retrieves an instance based on its ID: <class name>.show(<id>).
+        Usage: <class name>.show(<id>)
+        """
+        args = shlex.split(arg)
+
+        if len(args) != 3 or args[1] != 'show(' or not args[2].endswith(')'):
+            print("** Invalid syntax. Usage: <class name>.show(<id>)")
+            return
+
+        class_name = args[0]
+        obj_id = args[2][:-1]  # Extract ID from the argument
+        key = "{}.{}".format(class_name, obj_id)
+        all_objs = storage.all()
+
+        if key in all_objs:
+            print(all_objs[key])
+        else:
+            print("** No instance found with ID: {}".format(obj_id))
+self.__class__.do_show_id = self.do_show_id
+
+    def do_destroy_id(self, arg):
+        """
+        Destroys an instance based on its ID: <class name>.destroy(<id>).
+        Usage: <class name>.destroy(<id>)
+        """
+        args = shlex.split(arg)
+
+        if len(args) != 3 or args[1] != 'destroy(' or not args[2].endswith(')'):
+            print("** Invalid syntax. Usage: <class name>.destroy(<id>)")
+            return
+
+        class_name = args[0]
+        obj_id = args[2][:-1]  # Extract ID from the argument
+        key = "{}.{}".format(class_name, obj_id)
+        all_objs = storage.all()
+
+        if key in all_objs:
+            del all_objs[key]
+            storage.save()
+        else:
+            print("** No instance found with ID: {}".format(obj_id))
+self.__class__.do_destroy_id = self.do_destroy_id
+
+    def update_by_id(self, arg):
+        """
+        Updates an instance based on its ID: <class name>.update(<id>, <attribute name>, <attribute value>).
+        Usage: <class name>.update(<id>, <attribute name>, <attribute value>)
+        """
+        args = shlex.split(arg)
+
+        if len(args) < 4 or args[1] != 'update(' or not args[-1].endswith(')'):
+            print("** Invalid syntax. Usage: <class name>.update(<id>, <attribute name>, <attribute value>)")
+            return
+
+        class_name = args[0]
+        obj_id = args[2]
+        attribute_name = args[3]
+        attribute_value = args[4][:-1]  # Extract value from the argument
+        key = "{}.{}".format(class_name, obj_id)
+        all_objs = storage.all()
+
+        if key not in all_objs:
+            print("** No instance found with ID: {}".format(obj_id))
+            return
+
+        obj = all_objs[key]
+        setattr(obj, attribute_name, attribute_value)
+        storage.save()
+self.__class__.update_by_id = self.update_by_id
+
+    def do_update_dict(self, arg):
+        """
+        Updates an instance based on its ID with a dictionary: <class name>.update(<id>, <dictionary representation>).
+        Usage: <class name>.update(<id>, <dictionary representation>)
+        """
+        args = shlex.split(arg)
+
+        if len(args) < 4 or args[1] != 'update(' or not args[-1].endswith(')'):
+            print("** Invalid syntax. Usage: <class name>.update(<id>, <dictionary representation>)")
+            return
+
+        class_name = args[0]
+        obj_id = args[2]
+        dictionary_str = args[3][1:-1]  
+        key = "{}.{}".format(class_name, obj_id)
+        all_objs = storage.all()
+
+        if key not in all_objs:
+            print("** No instance found with ID: {}".format(obj_id))
+            return
+
+        obj = all_objs[key]
+
+        try:
+            update_dict = eval(dictionary_str)
+        except Exception as e:
+            print("** Invalid dictionary representation: {}".format(e))
+            return
+
+        for attr, value in update_dict.items():
+            setattr(obj, attr, value)
+
+        storage.save()
+self.__class__.do_update_dict = self.do_update_dict
 
 
 if __name__ == '__main__':
